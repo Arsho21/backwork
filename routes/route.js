f677a4d9ed3530645fb274bf83849cc9c58cb407
Arsho21 <arshak.makaryan16@mail.ru>
2023-06-19 16:01:24 +0400

    first commit

---
+++ routes/route.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const { Todo, User } = require('../models/schema')
+
+router.get('/', (req, res) => {
+    res.send('armenia')
+})
+
+router.post('/create', async (req, res) => {
+    try {
+        const item = new Todo(req.body);
+
+        await item.save();
+        res.status(201).json({ message: 'okey' })
+    } catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+});
+
+
+router.post('/userCreate', async (req, res) => {
+    try {
+        const salt = bcrypt.genSaltSync(10);
+        const hashPassword = bcrypt.hashSync(req.body.password, salt);
+        const user = new User({...req.body, password: hashPassword});
+
+        const {password, ...others}=user._doc;
+
+        await user.save();
+        res.status(201).json(others)
+    } catch (e) {
+        res.status(400).json({ message: e.message })
+    }
+});
+
+
+router.delete('/delete/:id', async (req, res) => {
+   try{
+        const todo = await Todo.findByIdAndDelete(req.params.id)
+        res.status(200).json(todo)
+   }catch (e) {
+        res.status(400).json({ message: e.message})
+   }
+})
+
+
+
+module.exports = router;
---
+++ sever.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express');
+
+const mongoose = require('mongoose');
+const app = express();
+const router = require('./routes/route')
+
+
+app.use(express.json())
+
+app.use('/api', router)
+
+// const {User, Todo} = require('./models/schema'
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.once('open', () => {
+
+    console.log('connection to mongoDb');
+    app.listen(process.env.PORT, (err) => {
+        err ? console.log(err) : console.log('Server is listening');
+    })
+})
